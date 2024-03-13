aa2da6d011725c3e1270a4be54a5076a134dc63c
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-13 22:23:44 +0500

    configuring cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import 'cypress-xpath'
+
+/// <reference types = 'Cypress'/>
+/// <reference types = 'Cypress-xpath'/>
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-xpath'
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')

153977c9c75de27ed6715c40563575a6c5003648
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-17 22:49:00 +0500

    HTTP Post Method completed

--- cypress/e2e/1- Http-Requests/HttpRequests.cy.js
+++ cypress/e2e/1- Http-Requests/HttpRequests.cy.js
@@ -1,4 +1,4 @@
-//! HTTP REQUESTS
+//! HTTP REQUESTS (REST API)
 //! NOTE : use postman for the below API's to check the response and get better idea.   
 //* Here in this code we wanna send call to an API. 
 //* we are then getting its status code and put assertion on it to validate it is equal to 200. 
@@ -19,7 +19,7 @@ describe('Http Requests', ()=> {
     it('GET Call - Approach 2', ()=> {
 
         cy.request({
-            methode: 'GET',
+            method: 'GET',
             url: 'https://jsonplaceholder.typicode.com/posts'
         })
         .its('status').should('equal',200)    
@@ -27,12 +27,23 @@ describe('Http Requests', ()=> {
         //* usually we write methode & url / (everything in methode call) in single quotes, but in cypress it is not required.
     })
 
+    //! Yes, in RESTful APIs, the HTTP POST method is commonly used to create a new resource or instance on the server. 
+    //! When you send a POST request to a specific endpoint, 
+    //! you're telling the server to create a new resource with the provided data in the request body.
     it('POST Call', ()=> {
 
-        cy.request('GET', 'https://jsonplaceholder.typicode.com/posts')
-        .its('status')
-        .should('equal',200)
-
+        cy.request({
+            method: 'POST',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            body: {
+                    title: "Test Post",
+                    body: "My Name is Silent",
+                    userId: 1, 
+                }
+        })
+        .its('status').should('equal',201) 
 
     })
+
+    
 })

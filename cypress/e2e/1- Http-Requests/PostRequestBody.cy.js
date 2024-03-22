03a923cab65c74817194bc410784208cc1ca3ae4
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-22 23:17:43 +0500

    post request

---
+++ cypress/e2e/1- Http-Requests/PostRequestBody.cy.js
@@ -0,0 +1,12 @@
+//! There are 3 different ways in which we can create a post request body
+//* 1: we can hard code the data as part of the post request
+//* 2: we can dynamically or randomly generate the data
+//* 3: By using fixture concept in cypress
+
+//* we will use this api "https://reqres.in/api/users" to create a new record.
+/* {
+    "name": "morpheus",
+    "job": "leader"
+   }           */ 
+   
+//* we will send request like this but we have to change the name in order to create a new record.

5f7ff5795e20e42b7cce3e8b83ff57f1d35ad1d8
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-26 15:23:29 +0500

    header and cookies

---
+++ cypress/e2e/03. Header cookies and bearer token/header_cookies.cy.js
@@ -0,0 +1,10 @@
+//! headers, cookies, bearer Token 
+// as part of the header we can pass token, sometimes cookies along with the request
+//! so i will use an API, which by sending request to will generate token.
+//! Note: the email should be different every time you hit the API. & the token will be generated.
+//! that token will be used as bearer token to send other type of requests which belong to the same API.
+
+//* after creating the token you can place order with it. by passing it as a bearer token in the request.
+//* you can also view the placed order by following the url with the order ID.
+//* first we will get token --> place an order --> view or get placed order
+

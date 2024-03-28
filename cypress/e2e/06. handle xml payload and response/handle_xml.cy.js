8477a8b88da20b48f171392924d4479f96b4595d
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-28 23:10:17 +0500

    xml payload & response

---
+++ cypress/e2e/06. handle xml payload and response/handle_xml.cy.js
@@ -0,0 +1,6 @@
+//* in this task we will post request using XML format in the body.
+//* the response to the request will be in XML format so we will parse it.
+//! install library for parsing xml to json. npm install xml2js
+
+//* we will make 2 calls. in first call we will post to add a pet. in response we will get ID and some other data.
+//* in the second call we can get that pet by using that ID in the API call.

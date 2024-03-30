b711e2004c9572fb65a42844c155b8549a44bf32
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-30 22:43:03 +0500

    API key auth

--- cypress/e2e/07. API authentications/api_auth.cy.js
+++ cypress/e2e/07. API authentications/api_auth.cy.js
@@ -48,7 +48,7 @@ describe('APIs Authentications', ()=> {
     //! i have already generated token so i will skip the part of generating it. for details look at '03. header cookies'
     //! also you have to pass bearer token in header.
     
-    it('Bearer Token auth', ()=> {
+    it.skip('Bearer Token auth', ()=> {
 
         const bearerToken = "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"
         cy.request({
@@ -64,6 +64,27 @@ describe('APIs Authentications', ()=> {
             expect(response.status).equal(200)
             expect(response.body[0].id).equal(757888148)
 
+        })
+    })
+
+    //* Actual API URL:  https://api.openweathermap.org/data/2.5/weather?q=Islamabad&appid=2896c9038a03da71c1c96f82c778ddb4
+
+    //! most of the times we pass api key in query parameters
+    it("API key Auth", ()=> {
+
+        cy.request({
+            
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            qs: {
+                q: 'Islamabad',
+                appid: "2896c9038a03da71c1c96f82c778ddb4"       //! Api key for authentication
+                }
+        })
+        .then( (response)=> {
+            
+            expect(response.status).to.eq(200)
+            expect(response.body.name).to.eq("Islamabad")
         })
     })
 })

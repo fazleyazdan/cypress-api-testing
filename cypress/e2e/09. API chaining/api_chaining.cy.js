0e2fb236c6237bb2d848594b7a3082e57e49c176
fazleyazdan <fazleyazdan345@gmail.com>
2024-04-06 22:15:00 +0500

    API Chaining

---
+++ cypress/e2e/09. API chaining/api_chaining.cy.js
@@ -0,0 +1,9 @@
+//! we send request to api 'A'. and it gives us response.
+//! now we use we response of api 'A' in a request call to another api 'B' and so on.
+//! this is called API chaining, when you use response of an API in a request call to another API to get resources.
+
+
+describe('API chaining JSON placeholder', ()=> {
+
+    it()
+})
